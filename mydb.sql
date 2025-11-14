a60b3260a34644ab1783ef9864a8c2d30cb424d7
ja666ob <jacobmanuel262@gmail.com>
2025-11-14 21:04:25 +0800

    Add files via upload

---
+++ mydb.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 14, 2025 at 01:57 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_customers` ()   BEGIN
+    SELECT * FROM customers;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `referral_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`customer_id`, `first_name`, `last_name`, `referral_id`) VALUES
+(1, 'Fred', 'Fish', NULL),
+(2, 'Larry', 'Lobster', 1),
+(3, 'Bubble', 'Bass', 2),
+(4, 'Poppy', 'Puff', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `employee_id` int(11) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `hourly_pay` decimal(5,2) DEFAULT NULL,
+  `job` varchar(25) DEFAULT NULL,
+  `hire_date` date DEFAULT NULL,
+  `supervisor_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`employee_id`, `first_name`, `last_name`, `hourly_pay`, `job`, `hire_date`, `supervisor_id`) VALUES
+(1, 'Eugene', 'Krabs', 25.50, 'manager', '2023-01-02', NULL),
+(2, 'Squidward', 'Tentacles', 15.00, 'cashier', '2023-01-03', 5),
+(3, 'Spongebob', 'Squarepants', 12.50, 'cook', '2023-01-04', 5),
+(4, 'Patrick', 'Star', 12.50, 'cook', '2023-01-05', 5),
+(5, 'Sandy', 'Cheeks', 17.25, 'asst. manager', '2023-01-06', 1),
+(6, 'Sheldon', 'Plankton', 10.50, 'janitor', '2023-01-07', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `employee_attendance`
+-- (See below for the actual view)
+--
+CREATE TABLE `employee_attendance` (
+`first_name` varchar(50)
+,`last_name` varchar(50)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `transaction_id` int(11) DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `order_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`transaction_id`, `amount`, `customer_id`, `order_date`) VALUES
+(1000, 5.00, 3, '2023-01-01'),
+(1001, 3.00, 2, '2023-01-01'),
+(1002, 3.00, 3, '2023-01-02'),
+(1003, 5.00, 1, '2023-01-02'),
+(1004, 1.00, NULL, '2023-01-03'),
+(1005, 2.00, 4, '2023-01-03'),
+(1006, 6.00, NULL, '2023-01-03');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `employee_attendance`
+--
+DROP TABLE IF EXISTS `employee_attendance`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `employee_attendance`  AS SELECT `employees`.`first_name` AS `first_name`, `employees`.`last_name` AS `last_name` FROM `employees` ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD KEY `fk_customer_id` (`customer_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `fk_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
